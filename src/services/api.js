11efc052acf6fb3e6402303c867017461788b1da
rekha1907 <rekhandini190604@gmail.com>
2024-12-09 14:06:32 +0700

    tugasp3

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+export const fetchFoodData = async () => {
+    // Simulasi data dummy
+    return [
+      { id: 1, name: 'Nasi Goreng', image: null },
+      { id: 2, name: 'Sate Ayam', image: null },
+      { id: 3, name: 'Rendang', image: null },
+    ];
+  };
+  
